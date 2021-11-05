5a67fc76751bc8d58d15981fe2e3f95b2e7f19ef
David Schneider <d.schneider@fixpunkt.com>
2021-11-05 17:30:44 +0100

    file upload

--- comment/src/index.ts
+++ comment/src/index.ts
@@ -1,10 +1,12 @@
-import express, { NextFunction } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { query, body, validationResult } from 'express-validator';
 import process from 'process'
 import morgan from 'morgan'
 import mysql, { Connection } from 'mysql2/promise'
 import fs from 'fs/promises'
 import path from 'path'
+import { v4 as uuidv4 } from 'uuid'
+import Busboy from 'busboy'
 
 const MYSQL_URI = process.env.MYSQL_URI as string
 const PORT = process.env.PORT || 80
@@ -15,7 +17,7 @@ const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req : any, res : any, next : NextFunction) => {
+app.use((req : Request, res : Response, next : NextFunction) => {
   // set the CORS policy and CORS headers
   res.header('Access-Control-Allow-Origin', '*')
      .header('Access-Control-Allow-Headers', 'origin, X-Requested-With, Content-Type, Accept, Authorization')
@@ -29,29 +31,63 @@ app.use((req : any, res : any, next : NextFunction) => {
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', query('owner').isInt().optional(), (req : any, res : any, next : NextFunction) => {
+app.get('/', query('owner').isString().optional(), (req : any, res : any, next : NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', errors: errors.array() });
 
   connection.query(req.query.owner ? 'SELECT * FROM comments WHERE owner = ?' : 'SELECT * FROM comments', [req.query.owner])
-    .then(([rows]) => res.json(rows))
+    .then(([rows]) => res.json({ status: true, result: rows }))
     .catch(next)
 })
 
 app.get('/:path', (req : any, res : any, next : NextFunction) => {
   connection.query('SELECT * FROM comments WHERE path = ?', [req.params.path])
-    .then(([rows]) => res.json(rows))
+    .then(([rows]) => res.json({ status: true, result: rows }))
     .catch(next)
 })
 
-app.post('/:path', body('owner').isInt(), body('content').isString(), (req : any, res : any, next : NextFunction) => {
+app.post('/:path', body('owner').isString(), body('content').isString(), (req : any, res : any, next : NextFunction) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', errors: errors.array() });
+  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', message: errors.array().map((e: any) => e.param + ':' + e.msg).join(', ') });
 
-  connection.execute('INSERT INTO comments (owner, path, content) values (?, ?, ?)', [req.body.owner, req.params.path, req.body.content])
-    .then(([result]) => res.json(result))
+  const id = uuidv4()
+
+  connection.execute('INSERT INTO comments (id, owner, path, content) values (?, ?, ?, ?)', [id, req.body.owner, req.params.path, req.body.content])
+    .then(() => res.json({ success: true, result: id }))
     .catch(next)
 })
+
+app.put('/:id', (req : any, res : any, next : NextFunction) => {
+  var busboy = new Busboy({ headers: req.headers });
+
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+    
+    // file.pipe()
+    file.on('end', function() {
+      console.log('File [' + fieldname + '] Finished');
+    });
+  });
+
+  busboy.on('finish', function() {
+    console.log('Done parsing form!');
+    res.writeHead(303, { Connection: 'close', Location: '/' });
+    res.end();
+  });
+  req.pipe(busboy)
+})
+
+app.delete('/', body('id').isString(), (req : any, res : any, next : NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', message: errors.array().map((e: any) => e.param + ':' + e.msg).join(', ') });
+
+  connection.execute('DELETE comments WHERE id = ?', [req.body.id])
+    .then(() => res.json({ success: true, result: null }))
+    .catch(next)
+})
+
+/* Error Handler */
+app.use((err : Error, req : any, res : any) => res.status(500).json({ success: false, error: err.name, message: err.message }))
 
 mysql.createConnection(MYSQL_URI)
   .then(c => connection = c)
--- comment/src/tables.sql
+++ comment/src/tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS comments (
-    id int NOT NULL AUTO_INCREMENT,
-    owner int,
+    id varchar(128) NOT NULL,
+    owner varchar(256),
     path varchar(512) NOT NULL,
     content text,
 
