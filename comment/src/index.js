bb8caa0b4efbea37808a60762ad177532ad8f812
David Schneider <schule@davidschneider.xyz>
2021-11-05 17:44:12 +0100

    ts to js

--- comment/src/index.js
+++ comment/src/index.js
@@ -1,23 +1,25 @@
-import express, { NextFunction, Request, Response } from 'express'
-import { query, body, validationResult } from 'express-validator';
-import process from 'process'
-import morgan from 'morgan'
-import mysql, { Connection } from 'mysql2/promise'
-import fs from 'fs/promises'
-import path from 'path'
-import { v4 as uuidv4 } from 'uuid'
-import Busboy from 'busboy'
+'use strict'
 
-const MYSQL_URI = process.env.MYSQL_URI as string
+const express = require('express')
+const { query, body, validationResult } = require( 'express-validator' )
+const process = require( 'process' )
+const morgan = require( 'morgan' )
+const mysql = require( 'mysql2/promise' )
+const fs = require( 'fs/promises' )
+const path = require( 'path' )
+const { uuidv4: v4 } = require( 'uuid' )
+const Busboy = require( 'busboy' )
+
+const MYSQL_URI = process.env.MYSQL_URI
 const PORT = process.env.PORT || 80
 
-var connection : Connection
+var connection
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req : Request, res : Response, next : NextFunction) => {
+app.use((req, res, next) => {
   // set the CORS policy and CORS headers
   res.header('Access-Control-Allow-Origin', '*')
      .header('Access-Control-Allow-Headers', 'origin, X-Requested-With, Content-Type, Accept, Authorization')
@@ -31,7 +33,7 @@ app.use((req : Request, res : Response, next : NextFunction) => {
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', query('owner').isString().optional(), (req : any, res : any, next : NextFunction) => {
+app.get('/', query('owner').isString().optional(), (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', errors: errors.array() });
 
@@ -40,15 +42,15 @@ app.get('/', query('owner').isString().optional(), (req : any, res : any, next :
     .catch(next)
 })
 
-app.get('/:path', (req : any, res : any, next : NextFunction) => {
+app.get('/:path', (req, res, next) => {
   connection.query('SELECT * FROM comments WHERE path = ?', [req.params.path])
     .then(([rows]) => res.json({ status: true, result: rows }))
     .catch(next)
 })
 
-app.post('/:path', body('owner').isString(), body('content').isString(), (req : any, res : any, next : NextFunction) => {
+app.post('/:path', body('owner').isString(), body('content').isString(), (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', message: errors.array().map((e: any) => e.param + ':' + e.msg).join(', ') });
+  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', message: errors.array().map(e => e.param + ':' + e.msg).join(', ') });
 
   const id = uuidv4()
 
@@ -57,7 +59,7 @@ app.post('/:path', body('owner').isString(), body('content').isString(), (req : 
     .catch(next)
 })
 
-app.put('/:id', (req : any, res : any, next : NextFunction) => {
+app.put('/:id', (req, res, next) => {
   var busboy = new Busboy({ headers: req.headers });
 
   busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
@@ -77,9 +79,9 @@ app.put('/:id', (req : any, res : any, next : NextFunction) => {
   req.pipe(busboy)
 })
 
-app.delete('/', body('id').isString(), (req : any, res : any, next : NextFunction) => {
+app.delete('/', body('id').isString(), (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', message: errors.array().map((e: any) => e.param + ':' + e.msg).join(', ') });
+  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', message: errors.array().map(e => e.param + ':' + e.msg).join(', ') });
 
   connection.execute('DELETE comments WHERE id = ?', [req.body.id])
     .then(() => res.json({ success: true, result: null }))
@@ -87,13 +89,13 @@ app.delete('/', body('id').isString(), (req : any, res : any, next : NextFunctio
 })
 
 /* Error Handler */
-app.use((err : Error, req : any, res : any) => res.status(500).json({ success: false, error: err.name, message: err.message }))
+app.use((err, req, res) => res.status(500).json({ success: false, error: err.name, message: err.message }))
 
 mysql.createConnection(MYSQL_URI)
   .then(c => connection = c)
   .then(() => connection.connect())
   .then(() => fs.readFile(path.join(__dirname, 'tables.sql'), 'utf8'))
-  .then((sql: string) => connection.execute(sql))
+  .then(sql => connection.execute(sql))
   .then(() => app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${ PORT }`)
   }))
