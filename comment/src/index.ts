05f44f329c21ff298cbf1d9e06e46057dac81e72
David Schneider <d.schneider@fixpunkt.com>
2021-11-05 07:59:35 +0100

    validation & minified 2 lines

--- comment/src/index.ts
+++ comment/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express'
+import express, { NextFunction } from 'express'
+import { query, body, validationResult } from 'express-validator';
 import process from 'process'
 import morgan from 'morgan'
-import { AddressInfo } from 'net'
 import mysql, { Connection } from 'mysql2/promise'
 import fs from 'fs/promises'
 import path from 'path'
 
 const MYSQL_URI = process.env.MYSQL_URI as string
-const PORT = process.env.PORT || 80 as any
+const PORT = process.env.PORT || 80
 
 var connection : Connection
 
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res, next) => {
+app.use((req : any, res : any, next : NextFunction) => {
   // set the CORS policy and CORS headers
   res.header('Access-Control-Allow-Origin', '*')
      .header('Access-Control-Allow-Headers', 'origin, X-Requested-With, Content-Type, Accept, Authorization')
@@ -29,30 +29,36 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
+app.get('/', query('owner').isInt().optional(), (req : any, res : any, next : NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', errors: errors.array() });
+
   connection.query(req.query.owner ? 'SELECT * FROM comments WHERE owner = ?' : 'SELECT * FROM comments', [req.query.owner])
     .then(([rows]) => res.json(rows))
     .catch(next)
 })
 
-app.get('/:path', (req, res, next) => {
+app.get('/:path', (req : any, res : any, next : NextFunction) => {
   connection.query('SELECT * FROM comments WHERE path = ?', [req.params.path])
     .then(([rows]) => res.json(rows))
     .catch(next)
 })
 
-app.post('/:path', (req, res, next) => {
+app.post('/:path', body('owner').isInt(), body('content').isString(), (req : any, res : any, next : NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', errors: errors.array() });
+
   connection.execute('INSERT INTO comments (owner, path, content) values (?, ?, ?)', [req.body.owner, req.params.path, req.body.content])
-  .then(([result]) => res.json(result))
-  .catch(next)
+    .then(([result]) => res.json(result))
+    .catch(next)
 })
 
-const lconnection = mysql.createConnection(MYSQL_URI) as unknown as Promise<Connection>
-lconnection
-  .then(c => connection = c as Connection)
-  .then(_ => connection.connect())
-  .then(_ => fs.readFile(path.join(__dirname, 'tables.sql'), 'utf8'))
-  .then(sql => connection.execute(sql))
-  .then(_ => app.listen(PORT, () => {
+
+(mysql.createConnection(MYSQL_URI) as unknown as Promise<Connection>)
+  .then((c: Connection) => connection = c)
+  .then(() => connection.connect())
+  .then(() => fs.readFile(path.join(__dirname, 'tables.sql'), 'utf8'))
+  .then((sql: string) => connection.execute(sql))
+  .then(() => app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${ PORT }`)
   }))
