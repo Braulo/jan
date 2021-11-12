9c62c4e15f7991580cd6bd8ea687e3c0684583a7
David Schneider <d.schneider@fixpunkt.com>
2021-11-12 13:03:46 +0100

    shopping service

---
+++ shoppingservice/src/routes/member.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const express = require('express')
+const { query, body, validationResult } = require( 'express-validator' )
+const { v4: uuidv4 } = require( 'uuid' )
+
+const router = express.Router()
+
+router.get('/:member/families', (req, res, next) => {
+    req.mysql.query('SELECT * FROM familymembers WHERE member = ?', [req.params.member])
+        .then(([rows]) => res.json({ status: true, result: rows.map(_ => _.family) }))
+        .catch(next)
+})
+
+router.get('/:member', (req, res, next) => {
+    req.mysql.query('SELECT * FROM shoppinglist WHERE member = ?', [req.params.member])
+        .then(([rows]) => res.json({ status: true, result: rows }))
+        .catch(next)
+})
+
+module.exports = router
