f88c88dcc8adfcd77853e193693f6e2f180dd9ba
David Schneider <d.schneider@fixpunkt.com>
2021-11-15 13:46:22 +0100

    shopping

--- shoppingservice/src/routes/member.js
+++ shoppingservice/src/routes/member.js
@@ -14,7 +14,11 @@ router.get('/:member/families', (req, res, next) => {
 
 router.get('/:member', (req, res, next) => {
     req.mysql.query('SELECT * FROM shoppinglist WHERE member = ?', [req.params.member])
-        .then(([rows]) => res.json({ status: true, result: rows }))
+        .then(([rows]) => res.json({ status: true, result: {
+                progress: rows.map(_ => _.status).reduce((prev, cur, i, arr) => prev + cur, 0) / rows.length * 2,
+                items: rows
+            }
+        }))
         .catch(next)
 })
 
