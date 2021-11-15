c3086c38429b5af524a5bc7257a6ef05fb1baa1c
David Schneider <schule@davidschneider.xyz>
2021-11-15 15:39:23 +0100

    normalize

--- bclu/src/index.js
+++ bclu/src/index.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const express = require('express')
-const { query, body, validationResult } = require( 'express-validator' )
-const process = require( 'process' )
+const { query, body, validationResult } = require('express-validator')
+const process = require('process')
 const barcodelookup = require('barcodelookup')
-const morgan = require( 'morgan' )
+const morgan = require('morgan')
 
 const PORT = process.env.PORT || 80
 const API_KEY = process.env.API_KEY
@@ -14,8 +14,8 @@ const app = express()
 app.use(morgan('dev'))
 
 app.get('/', query('barcode').isString().isLength({ max: 20 }), (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', errors: errors.array() });
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) return res.status(400).json({ error: 'ValidationError', errors: errors.array() })
 
   barcodelookup.lookup({ barcode: req.query.barcode, key: API_KEY })
 })
